59c339750335611e9bb143ff7090b0fc6ce55bce
Ramazan Ceylan <46501683+ramazan9182@users.noreply.github.com>
2023-06-18 14:01:14 +0300

    Add files via upload

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ramazan Ceylan on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Restart/Utilities/AudioPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayer.swift
+//  Restart
+//
+//  Created by Ramazan Ceylan on 18.06.2023.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+  if let path = Bundle.main.path(forResource: sound, ofType: type) {
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+      audioPlayer?.play()
+    } catch {
+      print("Could not play the sound file.")
+    }
+  }
+}
